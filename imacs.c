be6010dbdad428dcafcebe63d98fb6f424ff7cbf
Jonas S Karlsson <jsk@yesco.org>
2015-10-06 23:44:49 +0000

    refactor to make it more independent, no globals, and be able to have several buffers, just not show them at this moment.

--- imacs.c
+++ imacs.c
@@ -28,30 +28,44 @@
   #define BUFF_SIZE (1024*24)
 #endif
 
-int buff_size = BUFF_SIZE;
-char* buff = NULL;
-char* buff_end = NULL;
+typedef struct {
+    char* buff;
+    char* end;
+    int size;
 
-int row = 0;
-int col = 0;
-int scroll = 0;
+    int row;
+    int col;
+    int scroll;
+    char* filename;
+    
+    int lines;
+    int columns;
+} imacs_buffer;
 
-char* filename = "README.md";
+imacs_buffer main_buff = {
+    .size = BUFF_SIZE,
+    .buff = NULL,
+    .end = NULL,
+    .filename =  "README.md",
+    .row = 0,
+    .col = 0,
+    .scroll = 0,
+    .lines = 24,
+    .columns = 80,
+};
 
-int lines = 24;
-int columns = 80;
-
-char* getpos(int r, int c) {
-    char* p = buff;
-    while (r > 0 && *p && p < buff_end) if (*p++ == '\n') r--;
-    while (c > 0 && *p && p < buff_end && *p++ != '\n') c--;
-    return p < buff_end ? p : (char*)buff_end-1;
+char* getpos(imacs_buffer* b, int r, int c) {
+    char* p = b->buff;
+    char* end = b->end;
+    while (r > 0 && *p && p < end) if (*p++ == '\n') r--;
+    while (c > 0 && *p && p < end && *p++ != '\n') c--;
+    return p < end ? p : (char*)end - 1;
 }
 
-int currentLineLength() {
-    char* start = getpos(row, 0);
+int currentLineLength(imacs_buffer* b) {
+    char* start = getpos(b, b->row, 0);
     char* p = start;
-    while (*p != '\n' && *p && p < buff_end) p++;
+    while (*p != '\n' && *p && p < b->end) p++;
     return (p - start);
 }
 
@@ -62,16 +76,19 @@ void f() { fflush(stdout); }
 void clear() { printf("\x1b[2J\x1b[H"); }
 void gotorc(int r, int c) { printf("\x1b[%d;%dH", r+1, c+1); }
 void inverse(int on) { printf(on ? "\x1b[7m" : "\x1b[m"); }
-void insert(char c) {
-    char* from = getpos(row, col);
-    memmove(from+1, from, strlen(from)+1); // buff+len-from ?
+void insert(imacs_buffer* b, char c) {
+    // TODO: check not overflow buffer
+    // if (from > b->end) return error("Buffer full!", "");
+    char* from = getpos(b, b->row, b->col);
+    memmove(from + 1, from, strlen(from) + 1); // buff+len-from ?
     *from = c;
 
     // TODO: make function and use for ^F ?
-    col++;
-    if (col > columns) {
-        col = 0;
-        row++;
+    // TODO: remove as it should be covered already...
+    b->col++;
+    if (b->col > b->columns) {
+        b->col = 0;
+        b->row++;
     }
 }
 
@@ -95,49 +112,53 @@ void error(char* msg, char* arg) {
     #else
     exit(1);
     #endif
-    // TODO:
 }
 
-int readfile(char* filename) {
+int readfile(imacs_buffer* b, char* filename) {
 #ifdef OTA
-    //strcpy(buff, "a file\nfor\nyou to\nedit!\n");
-    strncpy((char*)buff, (char*)README_md, sizeof(README_md));
-    return strlen(buff)+1;
+    strncpy((char*)b->buff, (char*)README_md, sizeof(README_md));
+    int len = strlen(buff) + 1;
 #else
+    if (!filename) return -1;
     FILE *f = fopen(filename, "r");
     if (!f) return error("No such file: ", filename), -1;
-    memset(buff, 0, buff_size);
-    int len = fread(buff, buff_size-1, 1, f);
+    memset(b->buff, 0, b->size);
+    int len = fread(b->buff, b->size - 1, 1, f);
     fclose(f);
-    return len;
 #endif
+    b->row = 0;
+    b->col = 0;
+    b->filename = filename;
+    return len;
 }
 
-void print_modeline(char* filename, int row, int col) {
-    gotorc(lines-2, 0);
+void print_modeline(imacs_buffer* b) {
+    gotorc(b->lines - 2, 0);
     inverse(1);
-    printf("-- imacs --**-- %-20s L%d C%d (text) --------------------------", filename, row, col);
+    printf("-- imacs --**-- %-20s L%d C%d (text) --------------------------",
+           b->filename, b->row, b->col);
     inverse(0);
     f();
 }
 
-void update(char* filename, int row, int col, int scroll) {
+void update(imacs_buffer* b) {
     clear(); // maybe can clear only edit area?
 
-    print_modeline(filename, row, col);
+    print_modeline(b);
 
     gotorc(0, 0);
     // print visible text from rows [scroll, scroll+lines-3]
-    char* p = getpos(scroll, 0);
-    char* end = getpos(scroll+lines-3, columns);
-    while (p < end) putchar(*p++);
+    char* p = getpos(b, b->scroll, 0);
+    char* endVisible = getpos(b, b->scroll + b->lines - 3, b->columns);
+    while (p < endVisible) putchar(*p++);
 
     // set cursor
-    gotorc(row - scroll, col); f();
+    gotorc(b->row - b->scroll, b->col);
+
+    f();
 }
 
-int getch()
-{
+int getch() {
 #ifdef OTA
     char c;
     read(0, (void*)&c, 1);
@@ -159,59 +180,67 @@ int getch()
 
 #define GETENV(name, default) ({ char* v = getenv(name); v ? atoi(v) : (default); })
 
-int main(int argc, char* argv[]) {
-#ifndef OTA
-    filename = argc > 1 ? argv[1] : "README.md";
+void imacs_init(imacs_buffer* b) {
+    b->size = BUFF_SIZE;
+    b->buff = (char*) calloc(1, BUFF_SIZE);
+    b->end = b->buff + b->size;
 
+#ifndef OTA
     // system specific
-    lines = GETENV("LINES", 24);
-    columns = GETENV("COLUMNS", 80);
+    b->lines = GETENV("LINES", 24);
+    b->columns = GETENV("COLUMNS", 80);
     signal(SIGINT, restoreTerminalAndExit);
 #endif
-    // init
-    buff_size = BUFF_SIZE;
-    buff = (char*) calloc(1, BUFF_SIZE);
-    buff_end = buff + buff_size;
+}
 
-    if (filename) readfile(filename);
+int main(int argc, char* argv[]) {
+    imacs_buffer* b = alloca(sizeof(imacs_buffer));
+    imacs_init(b);
+#ifndef OTA
+    if (argc > 1)
+        readfile(b, argv[1]);
+    else // LOL
+#endif
+    readfile(b, "README.md");
 
     // loop
-    update(filename, row, col, scroll);
+    update(b);
     int c;
     //while (read(0, &c, 1) > 0) { //esp?
     while ((c = getch()) > 0) {
-        int len = currentLineLength();
-        char* p = getpos(row, col);
+        int len = currentLineLength(b);
+        char* p = getpos(b, b->row, b->col);
         if (0) ;
-        else if (c == 'P' - 64) row--;
-        else if (c == 'N' - 64) row++;
-        else if (c == 'F' - 64) col++;
-        else if (c == 'B' - 64) col--;
-        else if (c == 'A' - 64) col = 0;
-        else if (c == 'E' - 64) col = len;
-        else if (c == 10 || c == 'J' - 64) { insert('J' - 64); col = 0; row++; }
+        else if (c == 'P' - 64) b->row--;
+        else if (c == 'N' - 64) b->row++;
+        else if (c == 'F' - 64) b->col++;
+        else if (c == 'B' - 64) b->col--;
+        else if (c == 'A' - 64) b->col = 0;
+        else if (c == 'E' - 64) b->col = len;
+        else if (c == 10 || c == 'J' - 64) { insert(b, 'J' - 64); b->col = 0; b->row++; }
         else if (c == 12) ; // redraw
-        else if (c == 'D' - 64) { memmove(p, p+1, strlen(p+1)+1); }
-        else if (c == 'H' - 64 && p > buff) { memmove(p-1, p, strlen(p)+1); col--; }
-        else if (c == 'K' - 64) { char* from = p-col+len+(col==len); memmove(p, from, strlen(from)+1); } 
+        else if (c == 'D' - 64) { memmove(p, p+1, strlen(p + 1) + 1); }
+        else if (c == 'H' - 64 && p > b->buff) { memmove(p-1, p, strlen(p) + 1); b->col--; }
+        else if (c == 'K' - 64) { char* from = p- b->col + len + (b->col == len); memmove(p, from, strlen(from) + 1); } 
         else
-            insert(c);
+            insert(b, c);
 
+        // fix out of bounds relative to content and screen, loop till no change
         int r, c, l;
         do {
-            r = row; c = col; l = len;
-            if (col > len) { col = 0; row++; }
-            if (col < 0 && row) { row--; col = currentLineLength(); }
-            if (row < scroll) scroll--;
-            if (col < 0) col = 0;
-            if (row < 0) row = 0;
-            if (scroll < 0) scroll = 0;
-            if (row-scroll > lines-3) scroll++;
-            len = currentLineLength();
-        } while (!(r == row && c == col && l == len));
-        if (col > len) { col = 0; row++; len = -1; } // never get called?
+            r = b->row; c = b->col; l = len;
+            if (b->col > len) { b->col = 0; b->row++; }
+            if (b->col < 0 && b->row) { b->row--; b->col = currentLineLength(b); }
+            if (b->row < b->scroll) b->scroll--;
+            if (b->col < 0) b->col = 0;
+            if (b->row < 0) b->row = 0;
+            if (b->scroll < 0) b->scroll = 0;
+            if (b->row - b->scroll > b->lines - 3) b->scroll++;
+            len = currentLineLength(b);
+        } while (!(r == b->row && c == b->col && l == len));
+        if (b->col > len) { b->col = 0; b->row++; len = -1; } // never get called?
 
-        update(filename, row, col, scroll);
+        update(b);
 
         //printf("\m\m [%d]%c%c\n", c, *p, *p); // enable to read keyboard codes
     }
